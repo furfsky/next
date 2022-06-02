9ae49feb70f6b142482dd1271bab73a604dc4f6c
aiden shi <aidenhohoho@gmail.com>
2022-06-02 17:39:00 +0000

    truncate the minor changes from versions

--- components/downloads/config/versions.tsx
+++ components/downloads/config/versions.tsx
@@ -130,27 +130,27 @@ export const versions: Download[] = [
     },
   },
   {
-    version: 'v1.1.0',
+    version: 'v1.1',
     name: 'someone was too lazy to give it a title',
     variants: {
-      fl: '/files/v1.1.0/§r§lFurfSky §6§lReborn §8§lFL §8§l[§71.1.0§8§l].zip',
-      ov: '/files/v1.1.0/§r§lFurfSky §6§lReborn §8§lOV §8§l[§71.1.0§8§l].zip',
+      fl: '/files/v1.1/§r§lFurfSky §6§lReborn §8§lFL §8§l[§71.1.0§8§l].zip',
+      ov: '/files/v1.1/§r§lFurfSky §6§lReborn §8§lOV §8§l[§71.1.0§8§l].zip',
     },
   },
   {
-    version: 'v1.0.0',
+    version: 'v1.0',
     name: 'FULL RELEASE',
     variants: {
-      fl: '/files/v1.0.0/§r§lFurfSky §c§lR§6§le§e§lb§a§lo§9§lr§d§ln §8§lFL §8§l[§71.0.0§8§l].zip',
-      ov: '/files/v1.0.0/§r§lFurfSky §c§lR§6§le§e§lb§a§lo§9§lr§d§ln §8§lOV §8§l[§71.0.0§8§l].zip',
+      fl: '/files/v1.0/§r§lFurfSky §c§lR§6§le§e§lb§a§lo§9§lr§d§ln §8§lFL §8§l[§71.0.0§8§l].zip',
+      ov: '/files/v1.0/§r§lFurfSky §c§lR§6§le§e§lb§a§lo§9§lr§d§ln §8§lOV §8§l[§71.0.0§8§l].zip',
     },
   },
   {
-    version: 'v0.2.0',
+    version: 'v0.2',
     name: ' F7 and Jacob Events',
     variants: {
-      fl: '/files/v0.2.0/§6Furf§5Sky §eReborn §8§lFL [§70.2.0.§8§l].zip',
-      ov: '/files/v0.2.0/§6Furf§5Sky §eReborn §8§lOV [§70.2.0.§8§l].zip',
+      fl: '/files/v0.2/§6Furf§5Sky §eReborn §8§lFL [§70.2.0.§8§l].zip',
+      ov: '/files/v0.2/§6Furf§5Sky §eReborn §8§lOV [§70.2.0.§8§l].zip',
     },
   },
 ];
