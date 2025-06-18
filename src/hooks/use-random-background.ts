0932a45c006151b6c56e60832bf9cfd1833b68e0
Szymon Pel <szymonpel@proton.me>
2025-06-18 10:43:47 +0200

    Adapt useRandomBackground to API Route
    https://vercel.com/guides/loading-static-file-nextjs-api-route

--- src/hooks/use-random-background.ts
+++ src/hooks/use-random-background.ts
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
 
-const IMAGES_DIR = path.join(process.cwd(), "public", "backgrounds");
+const IMAGES_DIR = path.join(process.cwd(), "app", "public", "backgrounds");
 const images = fs.readdirSync(IMAGES_DIR);
 
 export const useRandomBackground = () => `/backgrounds/${images[Math.floor(Math.random() * images.length)]}`;
